85b3804b0f35a88cebe6790ef547bd3d207b6862
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2024-09-01 17:02:12 +0100

    Initial Commit

---
+++ foodgo/foodgoApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodgoApp.swift
+//  foodgo
+//
+//  Created by Luiz Correa on 01/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct foodgoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
